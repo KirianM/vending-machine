01fdb6b75c093ba375b692826d42a3a8b22de63b
Kirian Murgadella <844565+KirianM@users.noreply.github.com>
2022-03-07 22:18:30 +0100

    Updated use case test

--- tests/VendorMachine/Machine/Application/MachineCoinsInserterTest.php
+++ tests/VendorMachine/Machine/Application/MachineCoinsInserterTest.php
@@ -4,28 +4,41 @@ namespace VendorMachine\Tests\Machine\Application;
 
 use Symfony\Component\Console\Tester\CommandTester;
 use VendorMachine\Machine\Application\MachineCoinsInserter;
-use VendorMachine\Machine\Domain\Balance;
+use VendorMachine\Machine\Domain\MachineRepository;
 use VendorMachine\Shared\Domain\Coin;
 use VendorMachine\Tests\UnitTestCase;
+use Mockery\MockInterface;
+use VendorMachine\Shared\Domain\Coins;
 
 class MachineCoinsInserterTest extends UnitTestCase
 {
+    private MachineRepository|MockInterface|null $repository;
     private MachineCoinsInserter $inserter;
 
     public function setUp(): void
     {
         parent::setUp();
 
-        $this->inserter = new MachineCoinsInserter();
+        $this->inserter = new MachineCoinsInserter($this->repository());
     }
-    /**
-     * @test
-     * @doesNotPerformAssertions
-    */
+    /** @test */
     public function it_should_not_throw_exceptions(): void
     {
-        $this->inserter->__invoke(new Balance([
+        $coins = new Coins([
             new Coin(0.05)
-        ]));
+        ]);
+
+        $this->repository()
+            ->shouldReceive('updateBalance')
+            ->with($coins)
+            ->once()
+            ->andReturnNull();
+
+        $this->inserter->__invoke($coins);
+    }
+
+    public function repository(): MachineRepository|MockInterface
+    {
+        return $this->repository = $this->repository ?? $this->mock(MachineRepository::class);
     }
 }
